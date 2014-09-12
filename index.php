522496b3ba6eaa779424c0ae14e682bda2b9cdf2
REDNBLACK <rb@f0w.org>
2014-09-12 16:28:27 +0400

    Updated index.php

--- index.php
+++ index.php
@@ -2,5 +2,22 @@
 ini_set('error_reporting', -1);
 ini_set('display_errors', 'On');
 require_once 'vendor/autoload.php';
-$url = new \Kinopoisk2Imdb\Generator('well');
-var_dump($url->generate());
+
+/* Generator */
+//$generator = new \Kinopoisk2Imdb\Generator('file.xls');
+//var_dump($generator->init());
+
+/* Resource Manager */
+//$resource = new \Kinopoisk2Imdb\ResourceManager('file.json');
+//$resource->init();
+//var_dump($resource->getData());
+//var_dump($resource->getOneRow());
+//var_dump($resource->getOneRow());
+//var_dump($resource->getSettings());
+//var_dump($resource->getSettings('filesize'));
+//var_dump($resource->getData());
+
+/* Client */
+$client = new \Kinopoisk2Imdb\Client();
+$client->searchMovie('Frozen', '2013');
+var_dump($client->parseHtml());
